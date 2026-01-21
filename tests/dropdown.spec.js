6506712619323da96cc4b7457b970e746e41e2f2
Kavitha95-123 <kavithasriram95@gmail.com>
2026-01-21 12:42:23 +0530

    first commit

---
+++ tests/dropdown.spec.js
@@ -0,0 +1,46 @@
+import { test,expect } from "@playwright/test";
+test.skip('drpodown',async({page})=>{
+    await page.goto('https://testautomationpractice.blogspot.com/')
+   const rows = await page.locator('#country').all();
+   console.log(rows)
+   for(const row of rows){
+    const alltext = await row.textContent()
+    console.log(alltext)
+    if(alltext.includes('India')){
+        await row.click()
+    }
+     break;
+   }
+   await page.waitForTimeout(3000)
+})
+test('drp1',async({page})=>{
+await page.goto('https://www.flipkart.com/travel/flights?param=Vijeth&otracker=clp_bannerads_6_4.bannerAdCard.BANNERADS_Vijeth_travel%2Fflights_WCPOWSFQULX6')
+
+    await page.locator('//input[@class = "afiehA ZvxNMK ZzRAN5 Eej6mw dIPldQ QRzexE"]').click()
+    const options = await page.locator('//div[@class="fgU7Kf DxUEFR"]/div').all()
+    let city = "Hyderabad"
+    for(const option of options){
+        const alltext = await option.textContent()
+        //onsole.log(alltext)
+        if(alltext.includes(city)){
+            await option.click()
+            await page.waitForTimeout(2000)
+            br
+});
+test('123',async({page})=>{
+await page.goto('https://www.flipkart.com/travel/flights?param=Vijeth&otracker=clp_bannerads_6_4.bannerAdCard.BANNERADS_Vijeth_travel%2Fflights_WCPOWSFQULX6')
+
+    await page.locator('//input[@class = "afiehA ZvxNMK ZzRAN5 Eej6mw rPbh97 QRzexE"]').click()
+    const alltext = (await page.locator('//div[@class="fgU7Kf DxUEFR"]/div').last()).all()
+    let tocity = "Mumbai"
+    for(let text of alltext){
+        const allcity = await text.textContent()
+        if(allcity.includes(tocity)){
+            await text.click()
+            await page.waitForTimeout(2000)
+            break;
+        }
+    }
+
+    
+})
---
+++ tests/practice.spec.js
@@ -0,0 +1,12 @@
+import { test,expect } from "@playwright/test";
+test('flipkart',async({page})=>{
+    await page.goto("https://www.flipkart.com/")
+    await page.getByText('Flight Bookings').click()
+    await page.locator('#ONE_WAY').check()
+    //await page.getByRole('textbox',{name:"From",exact:true}).click()
+    await page.locator('//input[@class="afiehA ZvxNMK ZzRAN5 Eej6mw dIPldQ QRzexE"]').fill("Mumbai")
+    await page.waitForTimeout(2000)
+    await page.locator('//input[@class="afiehA ZvxNMK ZzRAN5 Eej6mw rPbh97 QRzexE"]').fill("Hyderabad")
+    await page.waitForTimeout(2000)
+    await page.getByRole('textbox',{name:"21 Jan, Wed"}).fill("23 Jan,Fri")
+})  
